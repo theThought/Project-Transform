d2be033591cc367ac95a94870c88371c62cf7805
Richard Abrams <richard@purpleplanet.com>
2022-02-07 13:54:56 +0000

    Implement SASS using gulp
    For portability (i.e. when other developers are working on project) I have moved the SASS compilation step into the Gulp file and set a default watcher. This means the IDE is no longer responsible for any build work.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 
-function autoprefixer() {
-    var autoprefixer = require('autoprefixer');
-    var sourcemaps = require('gulp-sourcemaps');
-    var postcss = require('gulp-postcss');
+function buildStyles() {
+    var sass = require('gulp-sass')(require('sass')),
+        sourcemaps = require('gulp-sourcemaps'),
+        autoprefixer = require('autoprefixer'),
+        postcss = require('gulp-postcss');
 
-    return gulp.src('Survey/source/styles/*.css')
+    return gulp.src('Survey/source/styles/*.scss')
         .pipe(sourcemaps.init())
+        .pipe(sass.sync().on('error', sass.logError))
         .pipe(postcss([ autoprefixer({grid: 'autoplace'}) ]))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('Survey/styles'))
 }
 
-exports.autoprefixer = autoprefixer;
+exports.default = function() {
+    gulp.watch('Survey/source/styles/*.scss', buildStyles);
+};
