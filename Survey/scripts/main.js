ceef3bc6c800b2ffdb3f0f5de5125c4ece79bc72
Richard Abrams <richabramsuk@users.noreply.github.com>
2022-03-29 18:38:29 +0100

    Case correction for domReady
    Resolves problem when developing on case sensitive platforms (iOS)

--- Survey/scripts/main.js
+++ Survey/scripts/main.js
@@ -1,4 +1,4 @@
-require(['domReady'], function (domReady) {
+require(['domready'], function (domReady) {
     domReady(function () {
         // anything that needs to be run on DOM completion
     })
