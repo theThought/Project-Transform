4fa55683290ba799a51e2f51fe07965443701cee
Richard Abrams <richard@purpleplanet.com>
2022-05-18 15:31:32 +0100

    Updates progress bars JSON
    As per Kevin's JSON and comments on Clickup

--- Survey/scripts/o-progress.js
+++ Survey/scripts/o-progress.js
@@ -46,43 +46,52 @@ define(['component'],
 
         oProgress.prototype.sections = function (sections) {
             var self = this;
+            var sectionindex = 1;
 
             sections.forEach(function (section) {
-                self.element.appendChild(self.createSection(section));
+                self.element.appendChild(self.createSection(section, sectionindex));
+                sectionindex++;
             });
         }
 
-        oProgress.prototype.createSection = function (section) {
+        oProgress.prototype.createSection = function (section, sectionindex) {
             var self = this;
-            var pageindex = 1;
 
             var newSection = document.createElement('div');
             newSection.classList.add('m-progress-section');
 
-            if (section.title === this.properties.currentsection) {
+            if (sectionindex === this.properties.currentsection) {
                 newSection.classList.add('current-section');
+            }
+
+            if (section.pages.current === 0) {
+                newSection.classList.add('no-progress');
+            } else if (section.pages.current <= section.pages.total) {
+                newSection.classList.add('in-progress');
+            } else {
+                newSection.classList.add('complete');
             }
 
             newSection.title = section.title;
 
-            for (pageindex = 1; pageindex <= section.pages; pageindex++) {
-                newSection.appendChild(self.createPage(pageindex, section.pages, section.title));
+            for (var pageindex = 1; pageindex <= section.pages.total; pageindex++) {
+                newSection.appendChild(self.createPage(section, pageindex));
             }
 
             return newSection;
         }
 
-        oProgress.prototype.createPage = function (pageindex, pagecount, sectiontitle) {
+        oProgress.prototype.createPage = function (section, pageindex) {
             var newPage = document.createElement('div');
             newPage.classList.add('a-progress-page');
 
-            if (pageindex === this.properties.currentpage) {
+            if (pageindex === section.pages.current) {
                 newPage.classList.add('current-page');
             }
 
-            newPage.title = sectiontitle
+            newPage.title = section.title
                 + " (" + pageindex
-                + "/" + pagecount + ")";
+                + "/" + section.pages.total + ")";
 
             return newPage;
         }
