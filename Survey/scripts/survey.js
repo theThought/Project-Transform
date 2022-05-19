4c7238193eb96515ef6334da72bcadf23c2403a4
Richard Abrams <richard@purpleplanet.com>
2022-05-19 13:28:18 +0100

    Replaces app.components object with array
    This helps with an issue where components had duplicate IDs. Now everything is pushed onto the array stack we can at least guarantee that all components will be recorded.

--- Survey/scripts/survey.js
+++ Survey/scripts/survey.js
@@ -5,7 +5,7 @@
  */
 
 function Survey() {
-    this.components = {};
+    this.components = [];
     this.properties = {};
     this.initialvalues = {};
 }
@@ -15,79 +15,79 @@ Survey.prototype.registerComponent = function (componentType, id, group) {
     switch (componentType.toLowerCase()) {
         case 'oprogress':
             requirejs(['o-progress'], function (oProgress) {
-                app.components[id] = new oProgress(id, group);
+                app.components.push(new oProgress(id, group));
             });
             break;
         case 'ainputsinglelineedit':
             requirejs(['a-input-singlelineedit'], function (aInputSinglelineEdit) {
-                app.components[id] = new aInputSinglelineEdit(id, group);
+                app.components.push(new aInputSinglelineEdit(id, group));
             });
             break;
         case 'moptionbase':
             requirejs(['m-option-base'], function (mOptionBase) {
-                app.components[id] = new mOptionBase(id, group);
+                app.components.push(new mOptionBase(id, group));
             });
             break;
         case 'abuttonoption':
             requirejs(['a-button-option'], function (aButtonOption) {
-                app.components[id] = new aButtonOption(id, group);
+                app.components.push(new aButtonOption(id, group));
             });
             break;
         case 'abuttonlistdropdown':
             requirejs(['a-button-list-dropdown'], function (aButtonListDropdown) {
-                app.components[id] = new aButtonListDropdown(id, group);
+                app.components.push(new aButtonListDropdown(id, group));
             });
             break;
         case 'ainputlistdropdown':
             requirejs(['a-input-list-dropdown'], function (aInputListDropdown) {
-                app.components[id] = new aInputListDropdown(id, group);
+                app.components.push(new aInputListDropdown(id, group));
             });
             break;
         case 'ainputmultilineedit':
             requirejs(['a-input-multilineedit'], function (aInputMultilineEdit) {
-                app.components[id] = new aInputMultilineEdit(id, group);
+                app.components.push(new aInputMultilineEdit(id, group));
             });
             break;
         case 'oquestiongrid':
             requirejs(['o-question-grid'], function (oQuestionGrid) {
-                app.components[id] = new oQuestionGrid(id, group);
+                app.components.push(new oQuestionGrid(id, group));
             });
             break;
         case 'oquestionlist':
             requirejs(['o-question-list'], function (oQuestionList) {
-                app.components[id] = new oQuestionList(id, group);
+                app.components.push(new oQuestionList(id, group));
             });
             break;
         case 'oquestion':
         case 'oquestionchoice':
         case 'oquestionsinglelineedit':
             requirejs(['o-question-choice'], function (oQuestionChoice) {
-                app.components[id] = new oQuestionChoice(id, group);
+                app.components.push(new oQuestionChoice(id, group));
             });
             break;
         case 'oquestioncontainer':
             requirejs(['o-question-container'], function (oQuestionContainer) {
-                app.components[id] = new oQuestionContainer(id, group);
+                app.components.push(new oQuestionContainer(id, group));
             });
             break;
         case 'oquestionhnumberslider':
             requirejs(['o-question-h-number-slider'], function (oQuestionHNumberSlider) {
-                app.components[id] = new oQuestionHNumberSlider(id, group);
+                app.components.push(new oQuestionHNumberSlider(id, group));
             });
             break;
         case 'abuttonpreterminator':
             requirejs(['a-input-button-dec'], function (aInputButtonDec) {
-                app.components[id] = new aInputButtonDec(id, group);
+                app.components.push(new aInputButtonDec(id, group));
             });
             break;
         case 'abuttonpostterminator':
             requirejs(['a-input-button-inc'], function (aInputButtonInc) {
-                app.components[id] = new aInputButtonInc(id, group);
+                app.components.push(new aInputButtonInc(id, group));
             });
             break;
         case 'alabelthumbvalue':
             requirejs(['a-label-thumbvalue'], function (aLabelThumbValue) {
-                app.components[id] = new aLabelThumbValue(id, group);
+                app.components.push(new aLabelThumbValue(id, group));
             });
             break;
         default:
