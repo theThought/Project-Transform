1c0874fb98024e4a08f46f6eba5e6222a3df77b6
Richard Abrams <richard@purpleplanet.com>
2022-05-19 13:29:25 +0100

    Prepare for better init()
    It is possible, on more complex pages, for the DOM to be ready before the app has finished building the component array. This checks and repeats until the page has completed. Note that this assumes there will always be at least one item registered in the component array.

--- Survey/scripts/main.js
+++ Survey/scripts/main.js
@@ -1,5 +1,27 @@
 require(['domready'], function (domReady) {
     domReady(function () {
+
+        var componentsReady = function () {
+
+            if (!app.components.length) {
+                // if the component array hasn't finished building but the DOM is complete
+                // we need to wait a moment for components to finish registering prior to init()
+                setTimeout(componentsReady, 300)
+            } else {
+                // ready to init() components
+                for (var component in app.components) {
+                    var currentcomponent = app.components[component];
+                    if (typeof currentcomponent['init'] === 'function') {
+                        currentcomponent.init();
+                    }
+                }
+
+            }
+        }
+
+        componentsReady();
+
+        // check whether there are any unused properties that might indicate a problem
         app.checkProperties();
     })
 });
