83fa1a5eb10fc0e00f597cc6be990c2ba00ab67d
Richard Abrams <richard@purpleplanet.com>
2022-05-19 19:06:09 +0100

    Button handling

--- Survey/scripts/a-button-option.js
+++ Survey/scripts/a-button-option.js
@@ -31,8 +31,9 @@ define(['component'],
             component.call(this, id, group);
 
             this.element = document.querySelector('input#' + this.id);
-            this.isExclusive = (this.element.getAttribute('data-exclusive') === 'true') || false;
+            this.isExclusive = (this.element.dataset.exclusive === 'true') || false;
 
+            this.configureButton();
             this.configureProperties();
             this.configureIncomingEventListeners();
             this.requestInitialSize();
@@ -45,10 +46,8 @@ define(['component'],
         aButtonOption.prototype.configureIncomingEventListeners = function () {
             // for each event listener there must be a corresponding event handler
             document.addEventListener("click", this, false);
-            document.addEventListener("change", this, false);
             document.addEventListener(this.group + "_enableExclusive", this, false);
             document.addEventListener(this.group + "_dismissExclusive", this, false);
-            document.addEventListener(this.group + "_textFocus", this, false);
             document.addEventListener(this.group + "_beginResize", this, false);
             document.addEventListener(this.group + "_endResize", this, false);
         }
@@ -57,9 +56,6 @@ define(['component'],
             switch (event.type) {
                 case "click":
                     this.onClick(event);
-                    break;
-                case "change":
-                    this.onChange(event);
                     break;
                 case this.group + "_enableExclusive":
                     this.onEnableExclusive(event);
@@ -76,20 +72,58 @@ define(['component'],
             }
         }
 
-        aButtonOption.prototype.onChange = function (event) {
-
+        aButtonOption.prototype.configureButton = function () {
+            if (!this.element.hasAttribute('data-checked')) {
+                this.element.setAttribute('data-checked', 'false');
+            }
         }
 
         aButtonOption.prototype.onClick = function (event) {
+            // stop buttons from submitting
+            if (event.target === this.element) {
 
+                event.preventDefault();
+
+                if (this.element.dataset.checked === 'true') {
+                    this.element.setAttribute('data-checked', 'false');
+                } else {
+                    this.element.setAttribute('data-checked', 'true');
+                }
+
+                // handle self-generated events
+                if (this.isExclusive && this.element.dataset.checked === 'true') {
+                    var enableExclusive = new CustomEvent(this.group + '_enableExclusive', {
+                        bubbles: true,
+                        detail: this
+                    });
+                    document.dispatchEvent(enableExclusive);
+
+                } else {
+                    var dismissExclusive = new CustomEvent(this.group + '_dismissExclusive', {
+                        bubbles: true,
+                        detail: this
+                    });
+                    document.dispatchEvent(dismissExclusive);
+                }
+
+            }
         }
 
         aButtonOption.prototype.onEnableExclusive = function (event) {
+
+            // handle external events
+            if (this.element !== event.detail.element) {
+                this.element.setAttribute('data-checked', 'false');
+            }
 
         }
 
         aButtonOption.prototype.onDismissExclusive = function (event) {
 
+            // handle external events
+            if (this.element !== event.detail.element && this.isExclusive) {
+                this.element.setAttribute('data-checked', 'false');
+            }
         }
 
         return aButtonOption;
