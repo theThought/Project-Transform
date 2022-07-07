7828749f42a1aeee7d021dc1f190c970e86a2afa
Richard Abrams <richard@purpleplanet.com>
2022-07-07 12:37:10 +0100

    Question instruction area follows size of question information in side-by-side layouts where the split is controlled by page properties.

--- Survey/scripts/page.js
+++ Survey/scripts/page.js
@@ -24,12 +24,18 @@ define(
         }
 
         page.prototype.sidebyside = function (width) {
-            var questionelements = this.element.getElementsByClassName('o-question-information');
+            var questioninformation = this.element.getElementsByClassName('o-question-information');
+            var questioninstruction = this.element.getElementsByClassName('o-question-errorandinstruction');
 
-            for (var i = 0; i < questionelements.length; i++) {
-                questionelements[i].style.flexBasis = width + '%';
-                questionelements[i].style.msFlex = '0 0 ' + width + '%';
+            for (var i = 0; i < questioninformation.length; i++) {
+                questioninformation[i].style.flexBasis = width + '%';
+                questioninformation[i].style.msFlex = '0 0 ' + width + '%';
             }
+
+            for (var i = 0; i < questioninstruction.length; i++) {
+                questioninstruction[i].style.maxWidth = width + '%';
+            }
+
         }
 
         return page;
