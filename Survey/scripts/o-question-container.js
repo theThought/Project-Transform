04cdb4adf11cd1afdb14f884dde139d080c25301
Richard Abrams <richard@purpleplanet.com>
2022-11-19 15:58:24 +0000

    WIP commit on rules
    This removes most of the original rule handling and has most of the content required for complex rule processing. The question values are not being inserted into the rule, so at present this is a breaking commit.

--- Survey/scripts/o-question-container.js
+++ Survey/scripts/o-question-container.js
@@ -29,12 +29,16 @@ define(['o-question'],
             oQuestion.call(this, id, group);
 
             this.element = document.querySelector('div[class~="o-question-container"][data-questiongroup="' + this.group + '"]');
+            this.collapse = false;
             this.complexVisibilityRule = '';
+            this.parsedVisibilityRule = '';
+            this.sourceQuestions = [];
 
             this.configureProperties();
             this.configureIncomingEventListeners();
-            this.configureVisibilityRules();
             this.configureInitialVisibility();
+            this.parseVisibilityRules();
+            this.processVisibilityRules();
             this.configurationComplete();
         }
 
@@ -49,7 +53,8 @@ define(['o-question'],
         oQuestionContainer.prototype.handleEvent = function (event) {
             switch (event.type) {
                 case 'broadcastChange':
-                    this.processVisibilityRules(event);
+                    this.debug('Calling process from broadcastChange');
+                    this.processVisibilityRules();
                     break;
                 case "configComplete":
                     this.onConfigurationComplete(event);
@@ -69,104 +74,77 @@ define(['o-question'],
         oQuestionContainer.prototype.configureInitialVisibility = function () {
 
             // if there are no visibility rules defined for this question lift the cover immediately
-            if (typeof this.properties.visible === "undefined"
-                && typeof this.properties.complexVisibility === "undefined") {
-                this.liftCover();
-            }
-
-            // proceed if we have found a visibility ruleset for this question
-            // this can either be an old-style ruleset or a complex rule string
-            if (typeof this.properties.visible === "object") {
-                this.element.classList.add('unavailable'); // initial visibility is hidden
-
-                if (this.collapse) {
-                    this.element.classList.add('collapse');
-                }
-
-                // at this point we need to iterate each rule and request relevant question values
-                // we do this by telling the contributing question to broadcast its current value
-                this.requestComponentValues();
-            }
-        }
-
-        oQuestionContainer.prototype.requestComponentValues = function () {
-            this.visibilityRules.forEach(function (rule) {
-                for (var component in app.components) {
-                    var groupname = app.components[component].group.toLowerCase();
-                    var questionrulename = rule.question.replace(/(\w)_([^qQ])/g, "$1__$2").toLowerCase();
-                    if (groupname.indexOf(questionrulename) !== -1) {
-                        app.components[component].broadcastChange();
-                    }
-                }
-            });
-        }
-
-        oQuestionContainer.prototype.configureVisibilityRules = function () {
             if (typeof this.properties.visible === "undefined") {
-                return false;
+                this.liftCover();
+                return;
             }
 
-            if (this.properties.visible.collapse === false) {
-                this.collapse = false;
+            // the collapse property removes the space required by the question if it is hidden
+            if (this.properties.visible.collapse === true) {
+                this.collapse = true;
+                this.element.classList.add('collapse');
             }
 
-            // if a complex visibility rules exists any old-style rules
-            // will not be added to the container for processing
-            if (this.properties.visible.complexrule !== undefined) {
-                this.parseComplexVisibilityRule(this.properties.visible.complexrule);
-            } else {
-                this.visibilityRules = this.properties.visible.rules;
-            }
+            // this question has visibility rules so should begin in the hidden state
+            this.element.classList.add('unavailable');
         }
 
-        oQuestionContainer.prototype.parseComplexVisibilityRule = function (ruleString) {
+        oQuestionContainer.prototype.parseVisibilityRules = function () {
+            if (typeof this.properties.visible === "undefined") {
+                return;
+            }
+
+            this.complexVisibilityRule = this.properties.visible.rules;
+            var ruleString = this.complexVisibilityRule;
             // regular expression that searches for a string followed by an operator
-            // operators are = < > <> .containsAny .containsNone .containsAll
-            var questionRe = /\s?(\w+)(\.containsAny|\.containsAll|\.containsNone|\s?[=<>]\s?)/;
-            var questions = ruleString.match(/%(.*?)%/g);
+            // operators are = < > <> .containsNone .containsNone .containsAll
+            var questionRe = /\s?(\w+)(\.contains(?:None|Any|All)\((.*?)\)|\s?[=<>]\s?)/;
+            var questions = ruleString.match(questionRe);
 
             if (questions === null) {
-                console.warn('A complex visibility rule was found but did not identify any questions:');
-                console.warn(ruleString);
+                this.debug('A visibility rule was found but did not identify any questions:', 2);
+                this.debug(ruleString, 2);
                 return;
             }
 
             ruleString = this.expandContainsAnyRule(ruleString);
             ruleString = this.expandContainsAllRule(ruleString);
             ruleString = this.expandContainsNoneRule(ruleString);
-            ruleString = this.getQuestionValues(ruleString);
             ruleString = this.replaceOperators(ruleString);
+            this.extractQuestionIdentifiers(ruleString);
+            ruleString = this.getQuestionValues(ruleString);
+            this.parsedVisibilityRule = ruleString;
+        }
 
-            console.info(ruleString);
+        oQuestionContainer.prototype.extractQuestionIdentifiers = function (ruleString) {
+            var questionRe = /%%(\w+)%%/g;
+            var questions = ruleString.match(questionRe);
 
-            var func = function (string) {
-                return (new Function('return (' + string + ')')());
+            for (var i = 0; i < questions.length; i++) {
+                var currentquestion = questions[i].replace('_', '__');
+                currentquestion = currentquestion.replace(questionRe, '_Q$1');
+                this.sourceQuestions.push(currentquestion);
             }
-
-            this.complexVisibilityRule = func(ruleString);
-            console.log(this.complexVisibilityRule);
         }
 
         oQuestionContainer.prototype.getQuestionValues = function (ruleString) {
+            var questionRe = /%%(\w+?)%%/g;
 
-            var questionRe = /\s?(\w+?)\./;
-            var questions = ruleString.match(/%(.*?)%/g);
-
-            for (var i = 0; i < questions.length; i++) {
-                var currentquestion = questions[i];
-                currentquestion = currentquestion.replace('_', '__');
+            for (var i = 0; i < this.sourceQuestions.length; i++) {
+                var currentquestion = this.sourceQuestions[i];
+                //currentquestion = currentquestion.replace('_', '__');
                 currentquestion = currentquestion.replace(questionRe, '_Q$1');
                 var questionelement = document.getElementsByName(currentquestion)[0];
                 var questionvalue = 0;
 
                 if (typeof questionelement === 'undefined') {
-                    console.warn('Could not find a question required by a visibility rule:');
-                    console.warn(currentquestion);
+                    this.debug('Could not find a question required by a visibility rule:', 2);
+                    this.debug(currentquestion, 2);
                 } else {
                     questionvalue = questionelement.value;
                 }
 
-                ruleString = ruleString.replace(/%(.*?)%/, '"' + questionvalue + '"');
+                ruleString = ruleString.replace(questionRe, "'" + questionvalue + "'");
             }
 
             return ruleString;
@@ -175,7 +153,9 @@ define(['o-question'],
         oQuestionContainer.prototype.replaceOperators = function (ruleString) {
             ruleString = ruleString.replace(/or/gi, '||');
             ruleString = ruleString.replace(/and/gi, '&&');
-            ruleString = ruleString.replace(/=/g, '===');
+            ruleString = ruleString.replace(/[^=]=[^=]/g, '===');
+            var questionRe = /\s?(\w+)(\s?[=<>]\s?)/;
+            ruleString = ruleString.replace(questionRe, "%%$1%%$2")
 
             return ruleString;
         }
@@ -185,119 +165,74 @@ define(['o-question'],
                 return ruleString;
             }
 
-            var expandedString = '';
-            var re = /%(.*?)%(\s+containsAny\((.*?)\))?/g;
-            var rules = ruleString.match(re);
+            var re = /\s?(\w+)\.containsAny\((.*)\)/g;
+            var matches;
+
             // match 0: full string
             // match 1: question
             // match 2: contains string
+            matches = re.exec(ruleString);
 
-            var contains = rules[2].split(',');
-            contains.forEach(function (item) {
-                expandedString += rules[1] + ' === ' + item + ' || ';
-            });
+            var expandedString = '%%' + matches[1] + '%% === ';
+            var contains = matches[2].split(',');
+            expandedString += contains.join(' || %%' + matches[1] + '%% === ');
 
-            return expandedString;
+            return '(' + expandedString + ')';
         }
 
         oQuestionContainer.prototype.expandContainsAllRule = function (ruleString) {
             if (ruleString.indexOf('containsAll') === -1) {
                 return ruleString;
             }
+
+            var re = /\s?(\w+)\.containsAll\((.*)\)/g;
+            var matches;
+
+            // match 0: full string
+            // match 1: question
+            // match 2: contains string
+            matches = re.exec(ruleString);
+
+            var expandedString = '%%' + matches[1] + '%% === ';
+            var contains = matches[2].split(',');
+            expandedString += contains.join(' && %%' + matches[1] + '%% === ');
+
+            return '(' + expandedString + ')';
         }
 
         oQuestionContainer.prototype.expandContainsNoneRule = function (ruleString) {
             if (ruleString.indexOf('containsNone') === -1) {
                 return ruleString;
             }
+
+            var re = /\s?(\w+)\.containsNone\((.*)\)/g;
+            var matches;
+
+            // match 0: full string
+            // match 1: question
+            // match 2: contains string
+            matches = re.exec(ruleString);
+
+            var expandedString = '!%%' + matches[1] + '%% === ';
+            var contains = matches[2].split(',');
+            expandedString += contains.join(' && !%%' + matches[1] + '%% === ');
+
+            return expandedString;
         }
 
-        oQuestionContainer.prototype.processVisibilityRules = function (event) {
-
-            // get data from the component that broadcast the state
-            // change event and started this process
-            var broadcastingComponent = event.detail;
-
-            // do not try and process visibility rules when the
-            // current component is the originating component
-            if (broadcastingComponent.id.indexOf(this.id) === 0) {
+        oQuestionContainer.prototype.processVisibilityRules = function () {
+            if (this.parsedVisibilityRule === '') {
                 return;
             }
 
-            // the required visibility score is equal to the number
-            // of rules to be processed, as they are 'AND' rules
-            var requiredScore = this.visibilityRules.length;
-            var currentScore = 0;
-
-            // if there are no rules to be processed and the
-            // question is already visible, there is nothing to do
-            if (requiredScore === 0 && this.available) {
-                return;
+            var func = function (string) {
+                return (new Function('return (' + string + ')')());
             }
 
-            // if there are no rules to be processed and the
-            // question is not visible, make it visible
-            if (requiredScore === 0 && !this.available) {
-                this.makeAvailable();
-                return;
-            }
-
-            // get context
-            var self = this;
-
-            // iterate and process the visibility rules
-            this.visibilityRules.forEach(function (rule) {
-                var ruleQuestion = rule.question.toLowerCase().replace(/(\w)_([^qQ])/g, "$1__$2");
-                var broadcastingComponentName = broadcastingComponent.group.toLowerCase().replace(/(\w)_([^qQ])/g, "$1_$2");
-
-                if (broadcastingComponentName.indexOf(ruleQuestion) !== -1) {
-
-                    if (rule.type === 'specific-option'
-                        && typeof broadcastingComponent.checkbox !== "undefined"
-                        && broadcastingComponent.checkbox.value.toLowerCase() !== rule.value.toLowerCase().replace(/(\w)_([^qQ])/g, "$1__$2")) {
-                        return;
-                    }
-
-                    // incoming event matches a ruleset, begin processing
-                    switch (rule.type) {
-                        case 'min-value':
-                            self.processVisibilityMinValue(rule, broadcastingComponent);
-                            break;
-                        case 'max-value':
-                            self.processVisibilityMaxValue(rule, broadcastingComponent);
-                            break;
-                        case 'not-value':
-                            self.processVisibilityNotValue(rule, broadcastingComponent);
-                            break;
-                        case 'specific-option':
-                            self.processVisibilitySpecificOption(rule, broadcastingComponent);
-                            break;
-                        case 'not-specific-option':
-                            self.processVisibilityNotSpecificOption(rule, broadcastingComponent);
-                            break;
-                        case 'specific-list':
-                            self.processVisibilitySpecificList(rule, broadcastingComponent);
-                            break;
-                    }
-
-                }
-            });
-
-            this.visibilityRules.forEach(function (rule) {
-                if (rule.satisfied) {
-                    currentScore++;
-                }
-            });
-
-            if (currentScore >= requiredScore) {
-                // the current question met its visibility criteria
-                this.makeAvailable();
-
-            } else if (this.available) {
-                // the current question has visibility rules and failed the criteria
-                this.makeUnavailable();
-            }
-
+            this.debug(this.complexVisibilityRule, 3);
+            this.debug(this.parsedVisibilityRule, 3);
+            var rule = func(this.parsedVisibilityRule);
+            this.debug(rule, 3);
         }
 
         oQuestionContainer.prototype.processVisibilityMinValue = function (rule, broadcastingComponent) {
@@ -428,7 +363,6 @@ define(['o-question'],
                 this.element.classList.add('no-separator');
             }
         }
-
 
         return oQuestionContainer;
 
