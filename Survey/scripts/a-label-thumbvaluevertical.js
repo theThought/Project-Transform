c540229a99ad8ff3e1ee5bfeefc6820cb6275caf
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2024-01-22 18:05:02 +0000

    Refactored vertical slider

--- Survey/scripts/a-label-thumbvaluevertical.js
+++ Survey/scripts/a-label-thumbvaluevertical.js
@@ -1,16 +1,19 @@
 define(['component'], function (component) {
 
+    // Constructor function
     function aLabelThumbValueVertical(id, group) {
         component.call(this, id, group);
 
         // For vertical slider
-        this.verticalElement = document.querySelector('div.o-question-vnumberslider[data-questiongroup=' + this.group + '] div.a-label-thumbvalue');
-        this.verticalSlider = document.querySelector('div.o-question-vnumberslider[data-questiongroup=' + this.group + '] input[type=range]');
+        this.verticalSlider = document.querySelector(`div.o-question-vnumberslider[data-questiongroup=${this.group}] input[type=range]`);
+        this.verticalElement = document.querySelector(`div.o-question-vnumberslider[data-questiongroup=${this.group}] div.a-label-thumbvalue`);
     }
 
+    // Inheritance setup
     aLabelThumbValueVertical.prototype = Object.create(component.prototype);
     aLabelThumbValueVertical.prototype.constructor = aLabelThumbValueVertical;
 
+    // Initialization method
     aLabelThumbValueVertical.prototype.init = function () {
         this.configureProperties();
         this.configureIncomingEventListeners();
@@ -23,50 +26,44 @@ define(['component'], function (component) {
         this.configurationComplete();
     };
 
+    // Event listener setup
     aLabelThumbValueVertical.prototype.configureIncomingEventListeners = function () {
-        // for each event listener there must be a corresponding event handler
+        // For each event listener, there must be a corresponding event handler
         if (this.verticalSlider) {
-            document.addEventListener(this.group + "_updateValue", this, false);
+            document.addEventListener(`${this.group}_updateValue`, this, false);
         }
     };
 
+    // Event handler
     aLabelThumbValueVertical.prototype.handleEvent = function (event) {
-        switch (event.type) {
-            case this.group + "_updateValue":
-                this.updateValue(event.detail);
-                break;
+        if (event.type === `${this.group}_updateValue`) {
+            this.updateValue(event.detail);
         }
     };
 
+    // Update value method
     aLabelThumbValueVertical.prototype.updateValue = function (eventDetail) {
-
         if (!eventDetail || !eventDetail.element) {
             // If eventDetail or eventDetail.element is null or undefined, do nothing
             return;
         }
 
-        var direction = (document.dir === 'rtl') ? 'rtl' : 'ltr';
-        var value = eventDetail.element.value;
-        var elementToUpdate;
+        // Destructuring for cleaner code
+        const { value, min = 0, max = 100 } = eventDetail.element;
+        const thumbWidth = 40;
+        const range = max - min;
 
-        elementToUpdate = this.verticalElement;
-    
-            elementToUpdate.innerHTML = value;
+        // Calculation of position and offsets
+        const position = ((value - min) / range) * 100;
+        const positionOffset = Math.round(thumbWidth * position / 100) - (thumbWidth / 2);
+        const positionPaddingOffset = Math.round(12 * position / 100) - 6;
 
-            var min = eventDetail.element.min ? eventDetail.element.min : 0;
-            var max = eventDetail.element.max ? eventDetail.element.max : 100;
-            var thumbWidth = 40;
-            var range = max - min;
+        // Element to update
+        const elementToUpdate = this.verticalElement;
 
-            var position = Number(((value - min) / range) * 100);
-            var positionOffset = Math.round(thumbWidth * position / 100) - (thumbWidth / 2);
-            var positionPaddingOffset = Math.round(12 * position / 100) - 6;
-
-            if (direction === 'rtl') {
-                elementToUpdate.style.right = 'calc(' + position + '% - ' + positionOffset + 'px - ' + positionPaddingOffset + 'px)';
-            } else {
-                elementToUpdate.style.left = 'calc(' + position + '% - ' + positionOffset + 'px - ' + positionPaddingOffset + 'px)';
-            }
+        // Applying styles and updating content
+        elementToUpdate.style.left = `calc(${position}% - ${positionOffset}px - ${positionPaddingOffset}px)`;
+        elementToUpdate.innerHTML = value;
     };
 
     return aLabelThumbValueVertical;
