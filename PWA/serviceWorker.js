5dd44c85ceea368f33e597e8048085a364ae3139
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2024-02-06 20:34:48 +0000

    Cors issues - project updated

--- PWA/serviceWorker.js
+++ PWA/serviceWorker.js
@@ -2,7 +2,7 @@ const staticCacheName = 'ipsos-pwa-static-cache-v1';
 const assets = [
   '/',
   '/index.html',
-  '/styles.css',
+  '/general-styles.css',
   '/script.js',
   '/favicon.ico',
   '/images/ipsos-pwa.png'
@@ -15,7 +15,7 @@ self.addEventListener('install', evt => {
             return cache.addAll(assets);
         })
     );
-    console.log('service worker has been installed');
+    console.log('Service worker has been installed');
 });
 
 self.addEventListener('activate', evt => {
@@ -28,13 +28,28 @@ self.addEventListener('activate', evt => {
             );
         })
     );
-    console.log('service worker has been activated');
+    console.log('Service worker has been activated');
 });
 
 self.addEventListener('fetch', evt => {
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                // Clone the response to add CORS headers
+                const clonedResponse = fetchRes.clone();
+                if (clonedResponse.ok) {
+                    const responseHeaders = new Headers(clonedResponse.headers);
+                    responseHeaders.append('Access-Control-Allow-Origin', '*');
+                    return new Response(clonedResponse.body, {
+                        status: clonedResponse.status,
+                        statusText: clonedResponse.statusText,
+                        headers: responseHeaders
+                    });
+                } else {
+                    // Return the original response if it's not OK
+                    return fetchRes;
+                }
+            });
         })
     );
 });
