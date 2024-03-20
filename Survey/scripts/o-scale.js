9024cdfe1b7af7cc186968c9a7b43cfdab904820
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2024-03-20 19:06:41 +0000

    updated the vertical scale

--- Survey/scripts/o-scale.js
+++ Survey/scripts/o-scale.js
@@ -1,7 +1,9 @@
-define(['o-question'], function (oQuestion) {
+define(['o-question'], 
+    function (oQuestion) {
+    console.log('oscallllleee');
     function oScale(id, group) {
         oQuestion.call(this, id, group);
-        
+        console.log('HELLLLOOOOO');
         console.log('oScale instantiated with id:', id, 'and group:', group);
     }
 
