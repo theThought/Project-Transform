febecb4a7583ac7993d28ef0e4a1515be9e493a9
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2024-03-21 16:36:33 +0000

    All of the scales completed.

--- Survey/scripts/o-scale.js
+++ Survey/scripts/o-scale.js
@@ -1,9 +1,7 @@
 define(['o-question'], 
     function (oQuestion) {
-    console.log('oscallllleee');
     function oScale(id, group) {
         oQuestion.call(this, id, group);
-        console.log('HELLLLOOOOO');
         console.log('oScale instantiated with id:', id, 'and group:', group);
     }
 
