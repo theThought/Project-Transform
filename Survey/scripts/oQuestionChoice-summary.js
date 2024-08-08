07d37c9d578caefb08c5754b2cb9a75b0f232f99
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2024-08-08 08:05:48 +0100

    question highlight
    Now using JS to solve the issue. Should be working now.

--- Survey/scripts/oQuestionChoice-summary.js
+++ Survey/scripts/oQuestionChoice-summary.js
@@ -1,13 +1,22 @@
 define(['o-question'], function (oQuestion) {
     function oQuestionChoiceSummary(id, group) {
-        // this.element = document.querySelector('.o-question-choice-summary');     
+        this.element = document.querySelector('.m-option-summary');
+        this.isLastAnswered = document.querySelector('.l-summary-lastchange');
     }
 
     oQuestionChoiceSummary.prototype = Object.create(oQuestion.prototype);
     oQuestionChoiceSummary.prototype.constructor = oQuestionChoiceSummary;
 
     oQuestionChoiceSummary.prototype.init = function () {
-        console.log('working');
+        if (this.isLastAnswered) {
+            var elements = document.querySelectorAll('.m-option-summary');
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                if (element.querySelector('.l-summary-lastchange')) {
+                    element.classList.add('l-summary-lastchange');
+                }
+            }
+        }
     };
 
     return oQuestionChoiceSummary;
