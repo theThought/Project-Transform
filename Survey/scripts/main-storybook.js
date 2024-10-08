14eb6d82bfaf2434e1974b375a2bf50e32f862c3
basher <basher@madasafish.com>
2024-10-08 10:20:28 +0100

    partial storybook component init fix - add new JS file

---
+++ Survey/scripts/main-storybook.js
@@ -0,0 +1,42 @@
+define(
+    function () {
+        var initInterval = 300;
+        var initRuns = 0;
+        var initEmpy = 0;
+        var initLimit = 10;
+
+        var initTimer = setInterval(componentsReady, initInterval);
+
+        function componentsReady () {
+            initRuns++;
+            var i = app.preinitcomponents.length;
+
+            if (i===0) {
+                initEmpy++;
+            } else {
+                console.info('Calling init for ' + i + ' components');
+            }
+
+            if (initEmpy >= initLimit) {
+                // clearInterval(initTimer);
+            }
+
+            while (i--) {
+                var currentcomponent = app.preinitcomponents[i];
+                if (typeof currentcomponent.init === 'function') {
+                    try {
+                        currentcomponent.init();
+                    } catch (e) {
+                        console.error('Failed to init component ' + currentcomponent.id + ' due to following error ' + e);
+                        console.log(e.stack);
+                    }
+                } else {
+                    console.log('No init defined for + ' + currentcomponent.id);
+                }
+                app.components.push(app.preinitcomponents.splice(i, 1)[0]);
+            }
+
+            // check whether there are any unused properties that might indicate a problem
+            app.checkProperties();
+        }
+    });
