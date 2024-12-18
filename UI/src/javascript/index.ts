517c1edbe150d665b2110de982ede1e8ee597a5a
basher <basher@madasafish.com>
2024-12-18 09:54:35 +0000

    simplify UI setup

--- UI/src/javascript/index.ts
+++ UI/src/javascript/index.ts
@@ -1,15 +1,24 @@
 // Import CSS.
 import '../../src/css/index.css';
 
-// Import config and UI module initialisation.
+// Import key ES modules.
 import { browserSupportsAllFeatures } from './config/browser-supports-features';
-import { uiInit } from './ui-init';
+import Survey from './survey';
+
+// Setup DEMO Web Component for Storybook introduction.
+import WCExample from './web-components/wc-example';
+!customElements.get('wc-example') &&
+    customElements.define('wc-example', WCExample);
 
 if (browserSupportsAllFeatures()) {
-    uiInit();
+    const app = new Survey();
+    app.init();
 } else {
     // Dynamic import polyfills, then instantiate UI modules.
     import('./utils/polyfills')
-        .then(() => uiInit())
+        .then(() => {
+            const app = new Survey();
+            app.init();
+        })
         .catch((e) => console.error(e));
 }
