49673a156fc37f649c118cecb2c1936f2e0f6af6
basher <basher@madasafish.com>
2025-01-23 10:20:02 +0000

    re-instate ACT survey folder from master branch

---
+++ Dimensions Surveys/ACT/containerlist.js
@@ -0,0 +1,172 @@
+var barcodelist = {
+    "list": [
+        {
+            "id": "beer_can",
+            "caption": "Beer can",
+            "category": "can",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Beer_can.svg"
+        },
+        {
+            "id": "beer_glass",
+            "caption": "Beer glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Beer_glass.svg"
+        },
+        {
+            "id": "beer_mug",
+            "caption": "Beer mug",
+            "category": "cup",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Beer_mug.svg"
+        },
+        {
+            "id": "beer",
+            "caption": "Beer bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Beer.svg"
+        },
+        {
+            "id": "Champagne_glass",
+            "caption": "Champagne glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Champagne_glass.svg"
+        },
+        {
+            "id": "Champagne",
+            "caption": "Champagne bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Champagne.svg"
+        },
+        {
+            "id": "Cognac_glass",
+            "caption": "Cognac glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Cognac_glass.svg"
+        },
+        {
+            "id": "Cognac",
+            "caption": "Cognac bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Cognac.svg"
+        },
+        {
+            "id": "Dessert_wine_glass",
+            "caption": "Dessert wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Dessert_wine_glass.svg"
+        },
+        {
+            "id": "Gin",
+            "caption": "Gin bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Gin.svg"
+        },
+        {
+            "id": "Highball_glass",
+            "caption": "Highball glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Highball_glass.svg"
+        },
+        {
+            "id": "Juice_glass",
+            "caption": "Juice glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Juice_glass.svg"
+        },
+        {
+            "id": "Martini_glass",
+            "caption": "Martini glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Martini_glass.svg"
+        },
+        {
+            "id": "Red_wine_glass",
+            "caption": "Red wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Red_wine_glass.svg"
+        },
+        {
+            "id": "Rum_glass",
+            "caption": "Rum glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Rum_glass.svg"
+        },
+        {
+            "id": "Rum_bottle",
+            "caption": "Rum bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Rum.svg"
+        },
+        {
+            "id": "Short_glass",
+            "caption": "Short glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Short_glass.svg"
+        },
+        {
+            "id": "Snifter",
+            "caption": "Snifter",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Snifter.svg"
+        },
+        {
+            "id": "Spirits",
+            "caption": "Spirits bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Spirits.svg"
+        },
+        {
+            "id": "Table_wine_glass",
+            "caption": "Table wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Table_wine_glass.svg"
+        },
+        {
+            "id": "Tequila",
+            "caption": "Tequila bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Tequila.svg"
+        },
+        {
+            "id": "Vodka_glass",
+            "caption": "Vodka glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Vodka_glass.svg"
+        },
+        {
+            "id": "Vodka",
+            "caption": "Vodka bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Vodka.svg"
+        },
+        {
+            "id": "Whiskey_glass",
+            "caption": "Whiskey glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Whiskey_glass.svg"
+        },
+        {
+            "id": "Whiskey",
+            "caption": "Whiskey bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Whiskey.svg"
+        },
+        {
+            "id": "White_wine_glass",
+            "caption": "White wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/White_wine_glass.svg"
+        },
+        {
+            "id": "Wine_box",
+            "caption": "Wine box",
+            "category": "box",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Wine_box.svg"
+        },
+        {
+            "id": "Wine",
+            "caption": "Wine bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/containers/Wine.svg"
+        }
+    ]
+}
---
+++ Dimensions Surveys/ACT/containerlistII.js
@@ -0,0 +1,172 @@
+var barcodelist = {
+    "list": [
+        {
+            "id": "beer_can",
+            "caption": "Beer can",
+            "category": "can",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Beer_can.svg"
+        },
+        {
+            "id": "beer_glass",
+            "caption": "Beer glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Beer_glass.svg"
+        },
+        {
+            "id": "beer_mug",
+            "caption": "Beer mug",
+            "category": "cup",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Beer_mug.svg"
+        },
+        {
+            "id": "beer",
+            "caption": "Beer bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Beer.svg"
+        },
+        {
+            "id": "Champagne_glass",
+            "caption": "Champagne glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Champagne_glass.svg"
+        },
+        {
+            "id": "Champagne",
+            "caption": "Champagne bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Champagne.svg"
+        },
+        {
+            "id": "Cognac_glass",
+            "caption": "Cognac glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Cognac_glass.svg"
+        },
+        {
+            "id": "Cognac",
+            "caption": "Cognac bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Cognac.svg"
+        },
+        {
+            "id": "Dessert_wine_glass",
+            "caption": "Dessert wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Dessert_wine_glass.svg"
+        },
+        {
+            "id": "Gin",
+            "caption": "Gin bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Gin.svg"
+        },
+        {
+            "id": "Highball_glass",
+            "caption": "Highball glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Highball_glass.svg"
+        },
+        {
+            "id": "Juice_glass",
+            "caption": "Juice glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Juice_glass.svg"
+        },
+        {
+            "id": "Martini_glass",
+            "caption": "Martini glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Martini_glass.svg"
+        },
+        {
+            "id": "Red_wine_glass",
+            "caption": "Red wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Red_wine_glass.svg"
+        },
+        {
+            "id": "Rum_glass",
+            "caption": "Rum glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Rum_glass.svg"
+        },
+        {
+            "id": "Rum_bottle",
+            "caption": "Rum bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Rum.svg"
+        },
+        {
+            "id": "Short_glass",
+            "caption": "Short glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Short_glass.svg"
+        },
+        {
+            "id": "Snifter",
+            "caption": "Snifter",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Snifter.svg"
+        },
+        {
+            "id": "Spirits",
+            "caption": "Spirits bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Spirits.svg"
+        },
+        {
+            "id": "Table_wine_glass",
+            "caption": "Table wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Table_wine_glass.svg"
+        },
+        {
+            "id": "Tequila",
+            "caption": "Tequila bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Tequila.svg"
+        },
+        {
+            "id": "Vodka_glass",
+            "caption": "Vodka glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Vodka_glass.svg"
+        },
+        {
+            "id": "Vodka",
+            "caption": "Vodka bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Vodka.svg"
+        },
+        {
+            "id": "Whiskey_glass",
+            "caption": "Whiskey glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Whiskey_glass.svg"
+        },
+        {
+            "id": "Whiskey",
+            "caption": "Whiskey bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Whiskey.svg"
+        },
+        {
+            "id": "White_wine_glass",
+            "caption": "White wine glass",
+            "category": "glass",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/White_wine_glass.svg"
+        },
+        {
+            "id": "Wine_box",
+            "caption": "Wine box",
+            "category": "box",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Wine_box.svg"
+        },
+        {
+            "id": "Wine",
+            "caption": "Wine bottle",
+            "category": "bottle",
+            "image": "https://cdn.ipsosinteractive.com/sandbox/kevin.gray/passiveobservation/act/images/multicontainers/Wine.svg"
+        }
+    ]
+}
